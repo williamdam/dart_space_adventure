7fa969f0aeaae403f9f0e6b2b9734ca9a46f33fd
William Dam <damw@oregonstate.edu>
2021-01-21 18:55:41 -0800

    planetData map to hold List of planets.

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -2,9 +2,25 @@ import 'package:dart_space_adventure/dart_space_adventure.dart';
 
 void main(List<String> arguments) {
 
+  // Hold system name
+  const systemName = 'Solar System';
+
+  // Map holds name:description pairs for planet data
+  const planetData = {
+    'Mercury': 'A very hot planet, closest to the sun.',
+    'Venus': 'It\'s very cloudy here!',
+    'Earth': 'There is something very familiar about this planet.',
+    'Mars': 'Known as the red planet.',
+    'Jupiter': 'A gas giant, with a noticeable red spot.',
+    'Saturn': 'This planet has beautiful rings around it.',
+    'Uranus': 'Strangely, this planet rotates around on its side.',
+    'Neptune': 'A very cold planet, furthest from the sun.',
+    'Pluto': 'I don\'t care what they say - it\'s a planet.'
+  };
+
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
-      name: 'Solar System', 
+      name: systemName, 
       planets: mockPlanets()
       )
     ).start();
