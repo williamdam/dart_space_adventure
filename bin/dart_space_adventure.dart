fd0d05b6f03c0912a59d2fb16fe5a108e713ea38
William Dam <damw@oregonstate.edu>
2021-01-21 19:39:06 -0800

    mockPlanets() builds list of planet objects from k-v pairs.

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,8 +1,6 @@
 import 'package:dart_space_adventure/dart_space_adventure.dart';
 
-void main(List<String> arguments) {
-
-  // Hold system name
+// Hold system name
   const systemName = 'Solar System';
 
   // Map holds name:description pairs for planet data
@@ -18,6 +16,8 @@ void main(List<String> arguments) {
     'Pluto': 'I don\'t care what they say - it\'s a planet.'
   };
 
+void main(List<String> arguments) {
+
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
       name: systemName, 
@@ -26,44 +26,11 @@ void main(List<String> arguments) {
     ).start();
 }
 
+// Build list of planet objects from key-value pairs
 List<Planet> mockPlanets() {
-
-  return [
-    Planet(
-      name: 'Mercury',
-      description: 'A very hot planet, closest to the sun.'
-    ),
-    Planet(
-      name: 'Venus',
-      description: 'It\'s very cloudy here!'
-    ),
-    Planet(
-      name: 'Earth',
-      description: 'There is something very familiar about this planet.'
-    ),
-    Planet(
-      name: 'Mars',
-      description: 'Known as the red planet.'
-    ),
-    Planet(
-      name: 'Jupiter',
-      description: 'A gas giant, with a noticeable red spot.'
-    ),
-    Planet(
-      name: 'Saturn',
-      description: 'This planet has beautiful rings around it.'
-    ),
-    Planet(
-      name: 'Uranus',
-      description: 'Strangely, this planet rotates around on its side.'
-    ),
-    Planet(
-      name: 'Neptune',
-      description: 'A very cold planet, furthest from the sun.'
-    ),
-    Planet(
-      name: 'Pluto',
-      description: 'I don\'t care what they say - it\'s a planet.'
-    )
-  ];
+  // Get entries from key-value pairs, map, then return objects
+  // Returns as iterable, use .toList() to convert to list
+  return planetData.entries.map( 
+    (e) => Planet(name: e.key, description: e.value)
+  ).toList();
 }
