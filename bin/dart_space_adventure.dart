d041b1696d9ce5552585d097795b8999a422414f
William Dam <damw@oregonstate.edu>
2021-01-21 20:21:32 -0800

    Reads json passed as arg and decode to local List.

--- bin/dart_space_adventure.dart
+++ bin/dart_space_adventure.dart
@@ -1,36 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:dart_space_adventure/dart_space_adventure.dart';
 
 // Hold system name
-  const systemName = 'Solar System';
+const systemName = 'Solar System';
 
-  // Map holds name:description pairs for planet data
-  const planetData = {
-    'Mercury': 'A very hot planet, closest to the sun.',
-    'Venus': 'It\'s very cloudy here!',
-    'Earth': 'There is something very familiar about this planet.',
-    'Mars': 'Known as the red planet.',
-    'Jupiter': 'A gas giant, with a noticeable red spot.',
-    'Saturn': 'This planet has beautiful rings around it.',
-    'Uranus': 'Strangely, this planet rotates around on its side.',
-    'Neptune': 'A very cold planet, furthest from the sun.',
-    'Pluto': 'I don\'t care what they say - it\'s a planet.'
-  };
-
+// Dynamic List stores planet data
+List<dynamic> planetData;
 void main(List<String> arguments) {
 
+  // Read in json file passed as arg, to string
+  final jsonString = File(arguments[0]).readAsStringSync();
+
+  // Get planetary system name from json
+  final systemName = jsonDecode(jsonString)['name'];
+  
+  // Get list of planets from json
+  planetData = jsonDecode(jsonString)['planets'];
+
+  // Instantiate SpaceAdventure object with data, and call start()
   SpaceAdventure(
     planetarySystem: PlanetarySystem(
       name: systemName, 
-      planets: mockPlanets()
+      planets: getPlanets()
       )
     ).start();
 }
 
-// Build list of planet objects from key-value pairs
-List<Planet> mockPlanets() {
+// Build list of planet objects from key-value pairs, using map function
+List<Planet> getPlanets() {
   // Get entries from key-value pairs, map, then return objects
   // Returns as iterable, use .toList() to convert to list
-  return planetData.entries.map( 
-    (e) => Planet(name: e.key, description: e.value)
+  return planetData.map( 
+    (planet) => Planet(name: planet['name'], description: planet['description'])
   ).toList();
 }
